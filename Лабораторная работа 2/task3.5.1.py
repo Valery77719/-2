224a653e59470e5a67cd047ff18af1220ee67cf2
Valery77719 <valerka.filimonova@gmail.com>
2024-11-24 23:56:34 +0500

    Лабораторная работа 2 (Функции.Методы )
    Во 2м задании понимаю, чтобы сравнить нужно сделать из строк множества и тогда их можно будет сравнить intersection, не могу догадаться как. Либо как-то по-отдельности сравнивать, но это займет много времени и неэффективно. В 3м задании как будто расстояние между строками не сходит или что, вобщем не пойму в чем ошибка. Если есть возможность, помогите пожалуйста

---
+++ Лабораторная работа 2/task3.5.1.py
@@ -0,0 +1,14 @@
+# TODO Напишите функцию для поиска индекса товара
+def sklad(items_list, tovar):
+    if tovar in items_list:
+        return items_list.index(tovar)
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = sklad(items_list, find_item )  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ Лабораторная работа 2/task3.5.2.py
@@ -0,0 +1,11 @@
+# TODO Напишите функцию find_common_participants
+list_=()
+def find_common_participants(participants_group):
+
+    print(participants_first_group.intersection(participants_second_group))
+
+    return find_common_participants(participants_first_group,participants_second_group)
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
